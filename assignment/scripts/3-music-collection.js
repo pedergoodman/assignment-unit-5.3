9eedb71ffe2026a89b1ff13a991415fae06dbf94
Peder Goodman <peder.goodman@gmail.com>
2023-06-01 17:43:07 -0500

    add findByArtist function and tests, add notes for expected test output

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -11,6 +11,7 @@ function addToCollection(title, artist, yearPublished) {
     collection.push(obj)
 }
 
+
 function showCollection(array) {
     console.log('This Collection has', array.length, 'albums.' );
     for (const i in array) {
@@ -19,6 +20,21 @@ function showCollection(array) {
 }
 
 
+function findByArtist(artist) {
+    let result = [];
+    for (const i in collection) {
+        if (collection[i].artist === artist) { // if artist argument === artist property value
+            result.push(collection[i])
+        }    
+    }
+    return result;
+}
+
+
+
+
+
+// TESTS
 
 addToCollection('Gliss Riffer', 'Dan Deacon', 2015);
 addToCollection("Fake Mews!", "Bad Bad Meow", 2017);
@@ -26,6 +42,18 @@ addToCollection('Half the Bad & Twice the Good', 'Bad Bad Meow', 2015);
 addToCollection('This is Happening', 'LCD Soundsystem', 2010);
 addToCollection('Lollygagger', 'Lollygagger', 2019);
 addToCollection('Soft Sounds From Another Planet', 'Japanese Breakfast', 2017);
+// should add each album to array
+
 
 console.log('collection is:', collection);
 console.log(showCollection(collection));
+// -> should return array with 6 objects
+
+
+console.log('Searching by Artist');
+console.log('Searching for Artist Dan Deacon', findByArtist('Dan Deacon'));
+// -> should return 1 obj
+console.log('Searching for Artist Bad Bad Meow', findByArtist('Bad Bad Meow'));
+// -> should return 2 obj
+console.log('Searching for Artist Andrew Bird', findByArtist('Andrew Bird'));
+// -> should return empty array

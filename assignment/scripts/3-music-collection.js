ed9ff03bec40d4cf38b4e1edbab3b75999d05cd3
Peder Goodman <peder.goodman@gmail.com>
2023-06-01 16:44:53 -0500

    Function to add to collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,3 +1,15 @@
 console.log('***** Music Collection *****')
 
 let collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+    let obj = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
+    }
+    collection.push(obj)
+}
+
+
+

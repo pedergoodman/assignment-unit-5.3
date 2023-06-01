674322a0e361eda62ce7d623c871b1ab56bdd549
Peder Goodman <peder.goodman@gmail.com>
2023-06-01 16:48:20 -0500

    add 6 albums, test collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -13,3 +13,14 @@ function addToCollection(title, artist, yearPublished) {
 
 
 
+
+
+
+addToCollection('Gliss Riffer', 'Dan Deacon', 2015);
+addToCollection("Fake Mews!", "Bad Bad Meow", 2017);
+addToCollection('Half the Bad & Twice the Good', 'Bad Bad Meow', 2015);
+addToCollection('This is Happening', 'LCD Soundsystem', 2010);
+addToCollection('Lollygagger', 'Lollygagger', 2019);
+addToCollection('Soft Sounds From Another Planet', 'Japanese Breakfast', 2017);
+
+console.log('collection is:', collection);

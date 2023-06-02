e938cc03db2c9131d6655356cfc0f9b1d0c6b9fe
Peder Goodman <peder.goodman@gmail.com>
2023-06-02 13:33:57 -0500

    formated tests, added lots of comments

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,34 +2,37 @@ console.log('***** Music Collection *****')
 
 let collection = [];
 
+
+// adds title, artist, year published, and an array of tracks to an object
 function addToCollection(title, artist, yearPublished, tracks) {
-    let obj = {
+    let album = {
         title: title,
         artist: artist,
         yearPublished: yearPublished,
         tracks: tracks
     }
-    collection.push(obj);
-}
+    collection.push(album); // pushes object into collection array
+} // end addToCollection
 
 
+// Displays collection length, then lists all the albums within the collection
 function showCollection(array) {
-    console.log('This Collection has', array.length, 'albums.' );
-    for (const i in array) {
+    console.log('This Collection has', array.length, 'albums.' ); 
+    for (const i in array) { // for every album in the array
         console.log(array[i].title + ' by ' + array[i].artist + ', published in ' + array[i].yearPublished);
     }
-}
+} // end showCollection
 
 
 function findByArtist(artist) {
     let result = [];
     for (const i in collection) {
-        if (collection[i].artist === artist) { // if artist argument === artist property value
+        if (collection[i].artist === artist) { // if artist in collection === artist property value, add to result
             result.push(collection[i]);
-        }    
+        }  // else do nothing  
     }
-    return result;
-}
+    return result; // returns artist(s) found, otherwise returns empty if notfound  
+} // end findByArtist
 
 
 function search(artist, year) {
@@ -44,7 +47,7 @@ function search(artist, year) {
     } else {
         return collection;
     }
-}
+} // end search
 
 function addTracks(trackName, trackDuration) {
     let result = {trackName: trackName, trackDuration: trackDuration};
@@ -54,10 +57,11 @@ function addTracks(trackName, trackDuration) {
 
 // TESTS
 
-// Adding Tracks
+// ADDING TRACKS
 
 // Defining tracks first before plugging into addToCollection()
 // first 3 use an array to add
+
 let glissRifferTracks = [
     {trackName:'Mind On Fire', trackDuration: '3:51'}, 
     {trackName:'When I Was Done Dying', trackDuration: '4:18'},
@@ -78,7 +82,8 @@ let halfBadTracks = [
     {trackName:'SCIENTISTS AND LAWMAKERS AGREE TO DISAGREE', trackDuration: '4:10'},
 ]
 
-// Test using a function to add tracks to an array before plugging into addToCollection()
+// Tests using a function to add tracks to an array before plugging into addToCollection()
+
 let lollygaggerTracks = [];
 lollygaggerTracks.push(addTracks('Microcosm', '3:42'));
 lollygaggerTracks.push(addTracks('Captain Chuckles VS. The Multiverse', '3:14'));
@@ -95,8 +100,9 @@ softSoundsTracks.push(addTracks('Planetary Ambience','1:17'));
 softSoundsTracks.push(addTracks('The Body Is a Blade','3:41'));
 
 
+// ADDING TO COLLECTION
 
-// should add each album to array
+// Should add each album to array
 addToCollection('Gliss Riffer', 'Dan Deacon', 2015, glissRifferTracks);
 addToCollection("Fake Mews!", "Bad Bad Meow", 2017, fakeMewsTracks);
 addToCollection('Half the Bad & Twice the Good', 'Bad Bad Meow', 2015, halfBadTracks);
@@ -105,28 +111,26 @@ addToCollection('Lollygagger', 'Lollygagger', 2019, lollygaggerTracks);
 addToCollection('Soft Sounds From Another Planet', 'Japanese Breakfast', 2017, softSoundsTracks);
 
 
-console.log('collection is:', collection);
-// should return array with 6 objects
-console.log(showCollection(collection));
+// Testing items in collection and showingCollection()
+console.log('collection is:', collection); // should return array with 6 objects
+showCollection(collection); // should return length of 6 and list all the albums  
 
 
-console.log('\tSearching by Artist');
-// should return an array with 1 objs
-console.log('Searching for Artist Dan Deacon', findByArtist('Dan Deacon'));
 
-// should return an array with 2 objs
-console.log('Searching for Artist Bad Bad Meow', findByArtist('Bad Bad Meow'));
+// SEARCHING
 
-// should return empty array
-console.log('Searching for Artist Andrew Bird', findByArtist('Andrew Bird'));
+console.log('\tSearching by Artist'); 
+
+// Searching for Artist
+console.log('Searching for Artist Dan Deacon', findByArtist('Dan Deacon')); // should return an array with 1 objs
+console.log('Searching for Artist Bad Bad Meow', findByArtist('Bad Bad Meow')); // should return an array with 2 objs
+console.log('Searching for Artist Andrew Bird', findByArtist('Andrew Bird')); // should return empty array
 
 
-console.log('Testing Search Function');
-// should return with one item in array
-console.log(search('Bad Bad Meow',2017));
-// should return with one item in array
-console.log(search('Bad Bad Meow',2015));
-// should return entire collection
-console.log(search('Bad Bad Meow',2012));
-// should return with one item in array
-console.log(search('Dan Deacon',2015));
+console.log('\tTesting Search Function');
+
+// Searching for band name and album year
+console.log("Searching for Bad Bad Meow, 2017", search('Bad Bad Meow',2017)); // should return with one item in array
+console.log("Searching for Bad Bad Meow, 2015", search('Bad Bad Meow',2015)); // should return with one item in array
+console.log("Searching for Bad Bad Meow, 2012", search('Bad Bad Meow',2012)); // should return entire collection
+console.log("Searching for Dan Deacon, 2015", search('Dan Deacon',2015)); // should return with one item in array
